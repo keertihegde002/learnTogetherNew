0a7d4f0950e62f2e9e0e463e6b03cb7a03d029d5
keerti <keertihegde002@gmail.com>
2023-05-23 16:42:59 +0530

    base

---
+++ collections_allfiles/base/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home , name="home"),
+    path('room/<str:pk>/',views.room,name="room"), 
+    path('create-room',views.create_room,name="create-room"),
+    path('update-room/<str:pk>',views.update_room,name="update-room"),
+    path('delete-room/<str:pk>',views.delete_room,name="delete-room"),
+    path('login',views.loginPage,name="login"),
+    path('logout',views.logoutPage,name="logout"),
+    path('register',views.registerUser,name="register"),
+    path('profile/<int:pk>',views.userProfile,name="user-profile"),
+    path('update-user',views.updateUser,name="user-update"),
+    path('topics',views.topicPage,name="topicpage"),
+    path('activity',views.activityPage,name="activity"),
+
+]
